e39e38b9673a315ddee4fef6196cc436b876c6d1
teampazi <paziteam@usepazi.com>
2024-09-06 10:14:49 -0400

    sessions and cookies

--- security-example/server.js
+++ security-example/server.js
@@ -6,11 +6,14 @@ const fs = require('fs');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
 
 const PORT = 3000;
 const config = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 }
 const AUTH_OTPIONS = {
   clientID: config.GOOGLE_CLIENT_ID,
@@ -26,15 +29,34 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 }
 
 passport.use(new Strategy(AUTH_OTPIONS, verifyCallback));
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 const app = express();
 
+// set security headers
 app.use(helmet());
+app.use(cookieSession({
+  name: 'node-learning-session',
+  keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+// initialize paspport for oauth2.0 authentication
 app.use(passport.initialize());
+app.use(passport.session());
+// use static files to send html, css, js files to the client
 app.use(express.static(path.join(__dirname, 'public')));
+// use json parser to parse request body
 app.use(express.json());
 
+// check if user is logged in
 const checkLoggedIn = (req, res, next) => {
-  const isLoggedIn = true;
+  const isLoggedIn = req.isAuthenticated() && req.user;
+  console.log('req.user', req.user);
+  
   if(!isLoggedIn) {
     return res.status(401).send({
       message: 'You are not authenticated!'
@@ -49,7 +71,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/auth/google', passport.authenticate("google",  {
-  scope: ['profile', 'email']
+  scope: ['profile', 'email'] 
 }), (req, res) => {
   res.status(200).send({
     message: 'Google authentication successful!'
@@ -59,7 +81,7 @@ app.get('/auth/google/callback',
   passport.authenticate('google', {
     successRedirect: '/',
     failureRedirect: '/failure',
-    session: false
+    session: true
   }), (req, res) => {
   // res.status(200).send({
   //   message: 'Google authentication successful!'
@@ -73,13 +95,12 @@ app.get('/failure', (req, res) => {
   });
 });
 app.get('/auth/logout', (req, res) => {
-  res.status(200).send({
-    message: 'Google authentication successful!'
-  });
+  req.logout();
+  return res.redirect('/');
 });
 
 app.get('/secrete', checkLoggedIn, (req, res) => {
-  res.json({ message: 'Hello from server!' });
+  res.status(200).send("This is a highly protected secrete!");
 });
 
 https.createServer({
